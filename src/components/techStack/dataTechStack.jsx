34a8557f234cbea365d8735f88e8f6d0e6690365
agusasaad <agusasaad1099@hotmail.com>
2024-11-27 19:08:20 -0300

    fix de la huevona de fer

--- src/components/techStack/dataTechStack.jsx
+++ src/components/techStack/dataTechStack.jsx
@@ -1,59 +1,59 @@
-import BackEnd from "@/assets/icons/BackEnd";
-import Crm from "@/assets/icons/Crm";
-import FrontEnd from "@/assets/icons/FrontEnd";
-import Gears from "@/assets/icons/Gears";
-import Infrastructure from "@/assets/icons/Infrastructure";
-import MagnifyingGlass from "@/assets/icons/MagnifyingGlass";
-import Storage from "@/assets/icons/Storage";
-import UxUi from "@/assets/icons/UxUi";
+import BackEnd from '@/assets/icons/BackEnd'
+import Crm from '@/assets/icons/Crm'
+import FrontEnd from '@/assets/icons/FrontEnd'
+import Gears from '@/assets/icons/Gears'
+import Infrastructure from '@/assets/icons/Infrastructure'
+import MagnifyingGlass from '@/assets/icons/MagnifyingGlass'
+import Storage from '@/assets/icons/Storage'
+import UxUi from '@/assets/icons/UxUi'
 
 export const techStackData = [
   {
-    title: "Desarrollo Frontend",
+    title: 'Desarrollo Frontend',
     description:
-      "Orientación experta para optimizar tu infraestructura y estrategia de TI, asegurando su alineación con los objetivos de tu negocio.",
-    icon: <FrontEnd width="30px" height="30px" color="white" />,
+      'Soluciones de desarrollo web a medida para crear sitios web responsivos y de alto rendimiento que atraigan y conviertan a tu audiencia.',
+    icon: <FrontEnd width='30px' height='30px' color='white' />,
   },
   {
-    title: "Desarrollo Backend",
+    title: 'Desarrollo Backend',
     description:
-      "Soluciones de desarrollo web a medida para crear sitios web responsivos y de alto rendimiento que atraigan y conviertan a tu audiencia.",
-    icon: <BackEnd width="30px" height="30px" color="white" />,
+      'Orientación experta para optimizar tu infraestructura y estrategia de TI, asegurando su alineación con los objetivos de tu negocio.',
+    icon: <BackEnd width='30px' height='30px' color='white' />,
   },
   {
-    title: "infraestructura",
+    title: 'infraestructura',
     description:
-      "Agiliza tus operaciones con soluciones de automatización que reducen el trabajo manual y aumentan la eficiencia.",
-    icon: <Infrastructure width="30px" height="30px" color="white" />,
+      'Agiliza tus operaciones con soluciones de automatización que reducen el trabajo manual y aumentan la eficiencia.',
+    icon: <Infrastructure width='30px' height='30px' color='white' />,
   },
   {
-    title: "Almacenamiento",
+    title: 'Almacenamiento',
     description:
-      "Soluciones avanzadas para gestionar y proteger tus datos de manera eficiente, garantizando accesibilidad y seguridad a largo plazo.",
-    icon: <Storage width="30px" height="30px" color="white" />,
+      'Soluciones avanzadas para gestionar y proteger tus datos de manera eficiente, garantizando accesibilidad y seguridad a largo plazo.',
+    icon: <Storage width='30px' height='30px' color='white' />,
   },
   {
-    title: "CRM",
+    title: 'CRM',
     description:
-      "Soluciones para gestionar y optimizar las relaciones con tus clientes, centralizando la información y mejorando la comunicación.",
-    icon: <Crm width="30px" height="30px" color="white" />,
+      'Soluciones para gestionar y optimizar las relaciones con tus clientes, centralizando la información y mejorando la comunicación.',
+    icon: <Crm width='30px' height='30px' color='white' />,
   },
   {
-    title: "Tecnologías de Automatización e Integración",
+    title: 'Tecnologías de Automatización e Integración',
     description:
-      "Agiliza tus operaciones con soluciones de automatización que reducen el trabajo manual y aumentan la eficiencia.",
-    icon: <Gears width="30px" height="30px" color="white" />,
+      'Agiliza tus operaciones con soluciones de automatización que reducen el trabajo manual y aumentan la eficiencia.',
+    icon: <Gears width='30px' height='30px' color='white' />,
   },
   {
-    title: "Product Analisis And Desing",
+    title: 'Análisis y Diseño de Productos',
     description:
-      "Comprendemos y transformamos las necesidades de nuestros clientes en soluciones tecnológicas innovadoras.",
-    icon: <MagnifyingGlass width="30px" height="30px" color="white" />,
+      'Comprendemos y transformamos las necesidades de nuestros clientes en soluciones tecnológicas innovadoras.',
+    icon: <MagnifyingGlass width='30px' height='30px' color='white' />,
   },
   {
-    title: "UX/UI Research and analysis",
+    title: 'Investigación y Análisis UX/UI',
     description:
-      "Diseño de interfaces intuitivas, fáciles de usar y visualmente atractivas, asegurando un producto funcional y optimizado.",
-    icon: <UxUi width="30px" height="30px" color="white" />,
+      'Diseño de interfaces intuitivas, fáciles de usar y visualmente atractivas, asegurando un producto funcional y optimizado.',
+    icon: <UxUi width='30px' height='30px' color='white' />,
   },
-];
+]
