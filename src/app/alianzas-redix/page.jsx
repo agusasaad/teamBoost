323d494f2b403a2c45164fef47bd8101c5676317
agusasaad <agusasaad1099@hotmail.com>
2025-07-22 12:57:13 -0300

    enhance styles for SectionCards and SeccionAlianzas; add new icon components for improved UI

--- src/app/alianzas-redix/page.jsx
+++ src/app/alianzas-redix/page.jsx
@@ -7,96 +7,116 @@ import MdOutlineRoute from '@/assets/icons/MdOutlineRoute'
 import BsDiagram3 from '@/assets/icons/BsDiagram3'
 import AiOutlineRobot from '@/assets/icons/AiOutlineRobot'
 import ChangeCircle from '@/assets/icons/ChangeCircle'
-// import Factory from '@/assets/icons/Factory'
-// import GiHealthNormal from '@/assets/icons/GiHealthNormal'
-// import ConveyorBelt from '@/assets/icons/ConveyorBelt'
+import UsersIcon from '@/assets/icons/UsersIcon'
+import Image from 'next/image'
+
+import logo_symbio from '@/assets/images/icon.svg'
+import logo_redix from '@/assets/images/logo-redix.svg'
+import Manos from '@/assets/icons/Manos'
+import ArrowCard from '@/assets/icons/ArrowCard'
 
 const AlianzasRoutes = () => {
-  const subtitleRef = useRef(null)
-  const titleRef = useRef(null)
-  const descriptionRef = useRef(null)
-
-  const beneficiosClaveData = [
+  const aplicacionesIndustrialesData = [
     {
-      name: 'Diagnóstico de madurez digital',
-      text: 'Diagnostica la madurez digital real de tu empresa.',
-      icon: <FiTrendingUp width='24px' height='24px' color='white' />,
-    },
-
-    {
-      name: 'Hoja de ruta estratégica',
-      text: 'Diseñar una hoja de ruta tecnológica y estratégica clara.',
-      icon: <MdOutlineRoute width='24px' height='24px' color='white' />,
-    },
-
-    {
-      name: 'Automatización inteligente',
-      text: 'Automatice procesos clave y conecte sistemas de forma inteligente.',
+      name: 'Diagnóstico estratégico personalizado',
+      text: 'Detectamos oportunidades de mejora en procesos clave, cultura organizacional y capacidades tecnológicas.',
       icon: <BsDiagram3 width='24px' height='24px' color='white' />,
     },
 
     {
-      name: 'Soluciones centradas en IA',
-      text: 'Implementar soluciones tecnológicas centradas en IA, eficiencia y resultados.',
-      icon: <AiOutlineRobot width='24px' height='24px' color='white' />,
-    },
-    {
-      name: 'Gestión del cambio',
-      text: 'Apoye a su equipo durante el cambio, garantizando la adopción y la sostenibilidad.',
+      name: 'Desarrollo de soluciones tecnológicas a medida',
+      text: 'Symbionet convierte esos hallazgos en sistemas ágiles, automatizados e impulsados por IA con las últimas y mejores tecnologías.',
       icon: <ChangeCircle width='24px' height='24px' color='white' />,
     },
+
+    {
+      name: 'Transformación integral, medible y con un equipo capacitado',
+      text: 'El resultado: eficiencia, trazabilidad y escalabilidad a largo plazo y un equipo acompañando toda la trazabilidad de la transformación digital de tu negocio.',
+      icon: <FiTrendingUp width='24px' height='24px' color='white' />,
+    },
   ]
-
-  // const aplicacionesIndustrialesData = [
-  //   {
-  //     name: 'Manufactura y bienes de consumo',
-  //     text: 'Transformar la producción y las experiencias del consumidor',
-  //     icon: <Factory width='24px' height='24px' color='white' />,
-  //   },
-
-  //   {
-  //     name: 'Salud y ciencias de la vida',
-  //     text: 'Innovar en la atención al paciente y las capacidades de investigación',
-  //     icon: <GiHealthNormal width='24px' height='24px' color='white' />,
-  //   },
-
-  //   {
-  //     name: 'Logística y venta minorista',
-  //     text: 'Optimizar las cadenas de suministro y los recorridos de los clientes',
-  //     icon: <ConveyorBelt width='24px' height='24px' color='white' />,
-  //   },
-  // ]
-
   return (
-    <div className={styles.container_home}>
-      <div className={styles.background}></div>
-      <div className={styles.container} id='home'>
-        <div className={styles.titles}>
-          <span className={styles.subtitle} ref={subtitleRef}>
-            <span className={styles.circle} />
-            SymbioNet & Redix
-          </span>
-          <h1 ref={titleRef}>
-            Una Alianza que combina estrategia, innovación y tecnología
-          </h1>
-          <p ref={descriptionRef}>
-            Con REDIX combinamos estrategia y tecnología para transformar las
-            organizaciones. Una colaboración diseñada para impulsar el impacto,
-            la eficiencia y el crecimiento sostenible.
-          </p>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <span className={styles.subtitle}>
+          <UsersIcon color='#10b981' />
+          Alianza Estratégica
+        </span>
+        <h1>Una sinergia pensada para generar impacto real</h1>
+        <p>
+          Nos unimos con <strong>Redix Consulting</strong>, una firma experta en
+          innovación estratégica y excelencia operativa, para ofrecer soluciones
+          tecnológicas IA que nacen desde un diagnóstico profundo del negocio.
+        </p>
+      </div>
 
-        <div className={styles.alianza_container}>
-          <SectionCards
-            mainTitle={'Beneficios clave'}
-            solutionsData={beneficiosClaveData}
-          />
-
-          {/* <SectionCards
-            mainTitle={'Aplicaciones industriales'}
-            solutionsData={aplicacionesIndustrialesData}
-          /> */}
+      <div className={styles.logos_con_alianza}>
+        <div className={styles.card}>
+          <div className={styles.container_img}>
+            <Image src={logo_symbio} width={40} height={40} alt='logo' />
+          </div>
+          <h3>SymbioNet</h3>
+          <span>Soluciones IA</span>
         </div>
+        <div className={styles.alinaza}>
+          <div className={styles.line}></div>
+          <i>
+            <Manos width='30px' height='30px' color='white' />
+          </i>
+          <div className={styles.line}></div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.container_img}>
+            <Image src={logo_redix} width={40} height={40} alt='logo' />
+          </div>
+
+          <h3>Redix Consulting</h3>
+          <span>Estrategia & Excelencia</span>
+        </div>
+      </div>
+
+      <div className={styles.text}>
+        <h2>
+          Esta alianza te acompaña a tomar decisiones de inversión tecnológica
+          con confianza, previsión y foco en el impacto.
+        </h2>
+        <p>Juntos, creamos una propuesta de valor única:</p>
+      </div>
+
+      <div className={styles.container_cards}>
+        <SectionCards solutionsData={aplicacionesIndustrialesData} />
+      </div>
+
+      <div className={styles.procesos}>
+        <h2>Proceso Integral</h2>
+
+        <div className={styles.container_card}>
+          <div className={styles.proceso}>
+            <span>1</span>
+            <h3>Detección del Problema</h3>
+            <p>Diagnóstico profundo del negocio</p>
+          </div>
+          <i>
+            <ArrowCard />
+          </i>
+          <div className={styles.proceso}>
+            <span>2</span>
+            <h3>Desarrollo de Solución</h3>
+            <p>Tecnología IA personalizada</p>
+          </div>
+          <i>
+            <ArrowCard />
+          </i>
+          <div className={styles.proceso}>
+            <span>3</span>
+            <h3>Solución Operativa</h3>
+            <p>Implementación y seguimiento</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.container_button}>
+        <button>¿Querés saber cómo podemos ayudarte?</button>
       </div>
     </div>
   )
